ffa4ef54326fc2c4d9b392ae2d0920b0e04e8a07
Ashita Singh <134136197+asiashita@users.noreply.github.com>
2024-07-30 04:56:54 +0000

    Sync LeetCode submission Runtime - 934 ms (64.80%), Memory - 40.2 MB (100.00%)

---
+++ 0507-perfect-number/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        if(num<1){
+            return false;
+            }
+        if(num%2!=0){
+            return false;
+        }
+        for (int i=1;i<num;i++){
+            
+        
+            if(num%i==0){
+                sum=sum+i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
