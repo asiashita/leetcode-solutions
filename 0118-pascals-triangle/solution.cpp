90aa243ba9e79387b0e08424fa84f85f703402ce
Ashita Singh <134136197+asiashita@users.noreply.github.com>
2025-07-08 05:28:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (63.22%)

---
+++ 0118-pascals-triangle/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int>GenRow(int row){
+        vector<int>ansRow;
+        ansRow.push_back(1);
+        long long ans=1;
+        for(int col=1;col<row;col++){
+            ans=ans*(row-col);
+            ans=ans/(col);
+            ansRow.push_back(ans);
+        }return ansRow;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(GenRow(i));
+            
+        }return ans;
+    }
+};
