6d85ddafc96739799792a75df9aa918e6eb54403
Ashita Singh <134136197+asiashita@users.noreply.github.com>
2025-09-11 15:48:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (34.66%)

---
+++ 0048-rotate-image/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+      int n=matrix.size();
+      //transpose
+      for(int i=0;i<=n-2;i++){
+        for(int j=i+1;j<=n-1;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+      }
+      //reverse the row
+      for(int i=0;i<=n-1;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+      }  
+    }
+};
