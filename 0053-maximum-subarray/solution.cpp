e415f4a133607f25cb00b42084d49b4eaffb8ed8
Ashita Singh <134136197+asiashita@users.noreply.github.com>
2025-09-11 08:59:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.8 MB (14.42%)

--- 0053-maximum-subarray/solution.cpp
+++ 0053-maximum-subarray/solution.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=0;int maxi=INT_MIN;
+        int sum=0;int maxi=INT_MIN;//kadane's algo
         for(int i=0;i<nums.size();i++){
+            if(sum<0)sum=0;
             sum+=nums[i];
             maxi=max(maxi,sum);
-            if(sum<0){
-                sum=0;
-            }
-        }return maxi;
+            
+            
+        }//if(maxi<0)return 0; for all negative integers in the array return 0(not mentioned in the question) 
+        return maxi;
     }
 };
+
