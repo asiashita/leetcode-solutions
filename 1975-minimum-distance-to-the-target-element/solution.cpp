ef223efc6c11dfe7da5da3c2e09c2e04945e8523
Ashita Singh <134136197+asiashita@users.noreply.github.com>
2026-04-13 13:50:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (50.52%)

---
+++ 1975-minimum-distance-to-the-target-element/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int x=0;
+        int mini=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                x=(abs(i-start));
+                mini=min(mini,x);
+                
+            }
+        }return mini;
+    }
+};
